2e397c05b002f3a230f4f8bbb162ac8a497c4a89
Derren Crawford <derren.crawford@gmail.com>
2019-03-20 13:59:40 +0000

    Added functionality

--- src/main/java/com/twobeardedguys/web/WebControl.java
+++ src/main/java/com/twobeardedguys/web/WebControl.java
@@ -7,16 +7,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.twobeardedguys.entity.User;
+import com.twobeardedguys.entity.Rideout;
 import com.twobeardedguys.repository.UserRepository;
+import com.twobeardedguys.repository.RideoutRepository;
 
 @Controller
 public class WebControl {
 	
 	@Autowired
+	private RideoutRepository rideoutRepo;
 	private UserRepository userRepo;
 	
+	@GetMapping("/")
+	public String root (ModelMap model) {
+		
+		return "landingPage";
+	}
+	
+
+	
+	@RequestMapping("/addRideout")
+	public String addRideout (ModelMap model) {
+		
+		List<Rideout> allRideouts = rideoutRepo.findAll();
+		Rideout rideout = new Rideout();
+		
+		model.put("allRideouts", allRideouts);
+		model.put("rideout", rideout);
+		
+		return "addRideout";
+	}
+	
+	@PostMapping("/addRideout")
+	public String rideoutPost (Rideout rideout) {
+		rideoutRepo.save(rideout);
+		
+		return"redirect:/";
+	}
+	
+/*
 
 	@GetMapping("/")
 	public String root (ModelMap model) {
@@ -30,10 +62,11 @@ public class WebControl {
 		return "register";
 	}
 	
-	@PostMapping("/")
-	public String rootPost (User user) {
+	@PostMapping("/removeme")
+	public String rootPostD (User user) {
 		userRepo.save(user);
 		
 		return"redirect:/";
 	}
+	*/
 }
